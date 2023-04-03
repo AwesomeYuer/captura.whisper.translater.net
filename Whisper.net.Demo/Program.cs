f60b041bc1aa4af512569e94d7c5f062c0127123
AwesomeYuer <AwesomeYuer@Microshaoft.com>
2023-04-03 18:10:04 +0800

    fix nullable warnings

--- Whisper.net.Demo/Program.cs
+++ Whisper.net.Demo/Program.cs
@@ -19,7 +19,7 @@ async Task Demo(Options opt)
     {
         Console.WriteLine($"Downloading Model {opt.ModelName}");
         using var modelStream = await WhisperGgmlDownloader.GetGgmlModelAsync(opt.ModelType);
-        using var fileWriter = File.OpenWrite(opt.ModelName);
+        using var fileWriter = File.OpenWrite(opt.ModelName!);
         await modelStream.CopyToAsync(fileWriter);
     }
 
@@ -40,17 +40,17 @@ async Task Demo(Options opt)
 
 void LanguageIdentification(Options opt)
 {
-    var bufferedModel = File.ReadAllBytes(opt.ModelName);
+    var bufferedModel = File.ReadAllBytes(opt.ModelName!);
 
     // Same factory can be used by multiple task to create processors.
     using var factory = WhisperFactory.FromBuffer(bufferedModel);
 
     var builder = factory.CreateBuilder()
-       .WithLanguage(opt.Language);
+       .WithLanguage(opt.Language!);
 
     using var processor = builder.Build();
 
-    using var fileStream = File.OpenRead(opt.FileName);
+    using var fileStream = File.OpenRead(opt.FileName!);
 
     var wave = new WaveParser(fileStream);
 
@@ -63,10 +63,10 @@ void LanguageIdentification(Options opt)
 async Task FullDetection(Options opt)
 {
     // Same factory can be used by multiple task to create processors.
-    using var factory = WhisperFactory.FromPath(opt.ModelName);
+    using var factory = WhisperFactory.FromPath(opt.ModelName!);
 
     var builder = factory.CreateBuilder()
-        .WithLanguage(opt.Language);
+        .WithLanguage(opt.Language!);
 
     if (opt.Command == "translate")
     {
@@ -75,7 +75,7 @@ async Task FullDetection(Options opt)
 
     using var processor = builder.Build();
 
-    using var fileStream = File.OpenRead(opt.FileName);
+    using var fileStream = File.OpenRead(opt.FileName!);
 
     await foreach (var segment in processor.ProcessAsync(fileStream, CancellationToken.None))
     {
@@ -86,16 +86,16 @@ async Task FullDetection(Options opt)
 public class Options
 {
     [Option('t', "command", Required = false, HelpText = "Command to run (lang-detect, transcribe or translate)", Default = "transcribe")]
-    public string Command { get; set; }
+    public string? Command { get; set; }
 
     [Option('f', "file", Required = false, HelpText = "File to process", Default = "kennedy.wav")]
-    public string FileName { get; set; }
+    public string? FileName { get; set; }
 
     [Option('l', "lang", Required = false, HelpText = "Language", Default = "auto")]
-    public string Language { get; set; }
+    public string? Language { get; set; }
 
     [Option('m', "modelFile", Required = false, HelpText = "Model to use (filename", Default = "ggml-base.bin")]
-    public string ModelName { get; set; }
+    public string? ModelName { get; set; }
 
     [Option('g', "ggml", Required = false, HelpText = "Ggml Model type to download (if not exists)", Default = GgmlType.Base)]
     public GgmlType ModelType { get; set; }
